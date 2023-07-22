4519c57867a3289a6c2d439e4dec0407bc2be861
Saifurrehman2303007KHIDEG <saif.khan@xloopdigital.com>
2023-07-22 22:43:37 +0500

    linear search

--- 04_Searching_Algorithms/linear_search.py
+++ 04_Searching_Algorithms/linear_search.py
@@ -1,1 +1,10 @@
-L = [10,92,23,74,55,36,17,86,95,30]
+array = [10,92,23,74,55,36,17,86,95,30]
+
+def linear_search(array, item):
+    for i in range(len(array)):
+        if array[i] == item:
+            return i
+    return 'not found'
+
+
+print(linear_search(array, 80))
